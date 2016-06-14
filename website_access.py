98421744a7a7dc16469bc28f060ccf74ffa7cc8a
Dhruv Paranjape <lord.dhruv@gmail.com>
2016-06-14 17:44:13 +0530

    Update website_access.py
    Complete rework.
    Added csv result file generation and reading range of ip addresses from csv.

--- website_access.py
+++ website_access.py
@@ -2,12 +2,19 @@ from requests import get
 from lxml.html import fromstring
 from datetime import datetime
 from requests.exceptions import ConnectionError
+from csv import writer, reader
+from itertools import islice
+import os
 
-website_list = "websites.txt"
-filename = "results.txt"
+website_list = "top-1m.csv"
+filename = "results.csv"
 cert_path = "SQTerminator.der"
-results = open(filename,'w')
-proxy = {'http':'http://10.11.11.1:3128'}
+websites = reader(open(website_list))
+results = writer(open(filename,"w"), dialect='excel')
+proxy = {
+    'http':'10.11.11.1:3128',
+    'https':'10.11.11.1:3128'
+    }
 
 #Start calculating program running time
 start_time = datetime.now().replace(microsecond=0)
@@ -17,53 +24,61 @@ website_counter = 0
 blocked_counter = 0
 not_blocked_counter = 0
 
+def Stats():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+    end_time = datetime.now().replace(microsecond=0)
+    duration = end_time - start_time
+    print("\n\t\t--Statistics--\n")
+    print("Total running time :\t\t", duration)
+    print("Total websites accessed :\t", website_counter)
+    print("Total websites blocked :\t", blocked_counter)
+    print("Total websites not blocked :\t", not_blocked_counter)
+
 #Header block
-results.write("WEBSITE NAME\t\tSTATUS\t\tBLOCKED SITE\t\tCATEGORY\n")
+results.writerow( ["WEBSITE NAME", "STATUS", "BLOCKED SITE", "CATEGORY"])
 
+start_row = int(input("Starting row number: "))
+end_row = int(input("Ending row number: "))
 
-for website in open(website_list,'r'):
-    website = website.rstrip("\n").rstrip("\r")
-    if website == '':
-	continue
+for website in islice(websites, start_row, end_row):
 
-    name = "http://"+website
+    name = "http://"+website[1]
     try :    
-	webpage = get(name)	#No proxy
-	webpage = get(name, proxies=proxy)	#with proxy enabled
-    	site_map = fromstring(webpage.content)
-    	website_counter = website_counter+1
+        webpage = get(name, proxies=proxy) # verify verify works else use cert=
+        #webpage = get(name, verify = cert_path)
+        #webpage = get(name, cert = cert_path)
+        site_map = fromstring(webpage.content)
+        website_counter = website_counter+1
     
-    	try:
-        	#Sites blocked
-        	result = site_map.xpath('//*[@id="content"]/div[2]/div/div[2]/div[1]/h1/label//text()')[0]
-        	site = site_map.xpath('//*[@id="content"]/div[2]/div/div[2]/div[1]/div/strong/text()[1]')[0].replace(": ","",1)
-        	category = site_map.xpath('//*[@id="content"]/div[2]/div/div[2]/div[1]/div/strong/text()')[1].replace(": ","",1)
-        	blocked_counter=blocked_counter + 1
+        try:
+            #Sites blocked
+            result = site_map.xpath('//*[@id="content"]/div[2]/div/div[2]/div[1]/h1/label//text()')[0]
+            result = "Blocked"
+            site = site_map.xpath('//*[@id="content"]/div[2]/div/div[2]/div[1]/div/strong/text()[1]')[0].replace(": ","",1)
+            category = site_map.xpath('//*[@id="content"]/div[2]/div/div[2]/div[1]/div/strong/text()')[1].replace(": ","",1)
+            blocked_counter=blocked_counter + 1
     
-    	except IndexError as e:
-        	#Oops sites not blocked
-        	result = "not blocked"
-        	site = ""
-        	category = ""
-        	not_blocked_counter = not_blocked_counter + 1
+        except IndexError as e:
+            #Oops sites not blocked
+            result = "--"
+            site = "--"
+            category = "--"
+            not_blocked_counter = not_blocked_counter + 1
 
     except ConnectionError :
-	#Oops sites not blocked
+    #Oops sites not blocked
         result = "not blocked"
         site = ""
         category = ""
         not_blocked_counter = not_blocked_counter + 1
 
     #Dumping into the file to have a report at the end
-    results.write(website+"\t"+result+"\t"+site+"\t"+category+"\n")
+    results.writerow([website[1], result, site, category])
 
-results.close()
+    if website_counter % 10 == 0:
+        Stats()
 
-end_time = datetime.now().replace(microsecond=0)
-
-duration = end_time - start_time
-print "\n\t\t--Statistics--\n"
-print "Total running time :\t\t", duration
-print "Total websites accessed :\t", website_counter
-print "Total websites blocked :\t", blocked_counter
-print "Total websites not blocked :\t", not_blocked_counter
+Stats()
